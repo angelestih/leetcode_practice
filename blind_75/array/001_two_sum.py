4f1b9f1a9f75f4f14ece8cc3cda06c89634e541c
angelestih <ade2119@columbia.edu>
2021-11-06 19:27:28 -0700

    Update 001_two_sum.py

--- blind_75/array/001_two_sum.py
+++ blind_75/array/001_two_sum.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    #brute force O(n^2)
+    def two_Sum(self, nums: List[int], target: int) -> List[int]:
+        for i, num1 in enumerate(nums):
+            for j, num2 in enumerate(nums):
+                if (num1+num2 == target):
+                    return [i, j]
+    #follow up using hash table to get O(n) worst case to go over all elements in array once
+    
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashTable= {}
+        for i, num in enumerate(nums):
+            if num in hashTable:
+                return [hashTable[num],i]
+            else:
+                hashTable[target - num] = i
