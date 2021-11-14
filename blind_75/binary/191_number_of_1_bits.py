53bbdb1657139d0a238d3b492393e3051635dfe2
angelestih <ade2119@columbia.edu>
2021-11-13 19:38:33 -0800

    deleted file

--- blind_75/binary/191_number_of_1_bits.py
+++ blind_75/binary/191_number_of_1_bits.py
@@ -5,6 +5,8 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         counter = 0
         for i in range(32):
+            # 1<<i will keep shifting 1 to the left 
+            #so that when anded with n, the reult would nbe 1 if there was a 1 in that position i
             if (n&(1<<i) !=0):
                 counter += 1
 
